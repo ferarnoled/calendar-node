8c565171c4651a28ef27dd860d0416e3dc9e95c8
fernando <abdala.fernando@beepi.com>
2017-07-11 13:12:57 -0700

    For debugging

--- www/app.js
+++ www/app.js
@@ -66,8 +66,8 @@ class App {
                 res.status(err.status || 500).json();
             }
             else {
-                winstonLogger.error(err);
                 res.status(err.status || 500).json(err);
+                winstonLogger.error(err);
             }
         });
 
